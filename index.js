eed4fd1d9596d784077df2c596ac41417d5168f9
Andrew <andrewryanogle@gmail.com>
2019-03-19 17:37:17 -0700

    added routes and knex config

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+};
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,8 +17,79 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const zoos = await db("zoos");
+    res.status(200).json(zoos); 
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-const port = 3300;
+server.get("/api/zoos/:id", async (req, res) => {
+  try {
+    const zoo = await db("zoos")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+const errors = {
+  "19": "Another record with that value exists"
+};
+
+server.post("/api/zoos", async (req, res) => {
+  try {
+    const [id] = await db("zoos").insert(req.body);
+
+    const zoo = await db("zoos")
+      .where({ id })
+      .first();
+    res.status(201).json(zoo);
+  } catch (error) {
+    res.status(500).json({ message: errors[error.errno] });
+  }
+});
+
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const role = await db("zoos")
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(role);
+    } else {
+      res.status(404).json({ message: "records not found" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+server.delete("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "records not found" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+const port = 9090;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
